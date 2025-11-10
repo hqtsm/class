5a7c369a06c98a0799fdd43b6fe9ef6b272c059b
Alexander O'Mara <me@alexomara.com>
2025-11-10 00:56:01 -0500

    Better examples

--- constant.test.ts
+++ constant.test.ts
@@ -8,7 +8,7 @@ type IsEqual<X, Y> = (
 ) ? true
 	: false;
 
-const constDesc = <T>(value: T) => ({
+const desc = <T>(value: T) => ({
 	value,
 	configurable: false,
 	enumerable: false,
@@ -17,64 +17,46 @@ const constDesc = <T>(value: T) => ({
 
 Deno.test('constant', () => {
 	class Abstract {
-		public static readonly staticPubRO = 1;
-		protected static readonly staticProRO = 2;
-		private static readonly staticPriRO = 3;
+		public static readonly PUB = 1;
+		protected static readonly PRO = 2;
+		private static readonly PRI = 3;
 
 		static {
-			constant(this, 'staticPubRO');
-			constant(this, 'staticProRO' as never);
-			constant(this, 'staticPriRO' as never);
+			constant(this, 'PUB');
+			constant(this, 'PRO' as never);
+			constant(this, 'PRI' as never);
 		}
 	}
 
 	class Concrete extends Abstract {
 		static {
-			constant(this, 'staticPubRO');
-			constant(this, 'staticProRO' as never);
-			constant(this, 'staticPriRO' as never);
+			constant(this, 'PUB');
+			constant(this, 'PRO' as never);
+			constant(this, 'PRI' as never);
 		}
 	}
 
-	void (true satisfies IsEqual<typeof Abstract['staticPubRO'], 1>);
-	void (true satisfies IsEqual<typeof Abstract['staticProRO'], 2>);
-	void (true satisfies IsEqual<typeof Abstract['staticPriRO'], 3>);
+	void (true satisfies IsEqual<typeof Abstract['PUB'], 1>);
+	void (true satisfies IsEqual<typeof Abstract['PRO'], 2>);
+	void (true satisfies IsEqual<typeof Abstract['PRI'], 3>);
 
-	void (true satisfies IsEqual<typeof Concrete['staticPubRO'], 1>);
-	void (true satisfies IsEqual<typeof Concrete['staticProRO'], 2>);
-	void (true satisfies IsEqual<typeof Concrete['staticPriRO'], 3>);
+	void (true satisfies IsEqual<typeof Concrete['PUB'], 1>);
+	void (true satisfies IsEqual<typeof Concrete['PRO'], 2>);
+	void (true satisfies IsEqual<typeof Concrete['PRI'], 3>);
 
-	assertEquals(Abstract.staticPubRO, 1);
-	assertEquals(Abstract['staticProRO'], 2);
-	assertEquals(Abstract['staticPriRO'], 3);
+	assertEquals(Abstract.PUB, 1);
+	assertEquals(Abstract['PRO'], 2);
+	assertEquals(Abstract['PRI'], 3);
 
-	assertEquals(Concrete.staticPubRO, 1);
-	assertEquals(Concrete['staticProRO'], 2);
-	assertEquals(Concrete['staticPriRO'], 3);
+	assertEquals(Concrete.PUB, 1);
+	assertEquals(Concrete['PRO'], 2);
+	assertEquals(Concrete['PRI'], 3);
 
-	assertEquals(
-		Object.getOwnPropertyDescriptor(Abstract, 'staticPubRO'),
-		constDesc(1),
-	);
-	assertEquals(
-		Object.getOwnPropertyDescriptor(Abstract, 'staticProRO'),
-		constDesc(2),
-	);
-	assertEquals(
-		Object.getOwnPropertyDescriptor(Abstract, 'staticPriRO'),
-		constDesc(3),
-	);
+	assertEquals(Object.getOwnPropertyDescriptor(Abstract, 'PUB'), desc(1));
+	assertEquals(Object.getOwnPropertyDescriptor(Abstract, 'PRO'), desc(2));
+	assertEquals(Object.getOwnPropertyDescriptor(Abstract, 'PRI'), desc(3));
 
-	assertEquals(
-		Object.getOwnPropertyDescriptor(Concrete, 'staticPubRO'),
-		constDesc(1),
-	);
-	assertEquals(
-		Object.getOwnPropertyDescriptor(Concrete, 'staticProRO'),
-		constDesc(2),
-	);
-	assertEquals(
-		Object.getOwnPropertyDescriptor(Concrete, 'staticPriRO'),
-		constDesc(3),
-	);
+	assertEquals(Object.getOwnPropertyDescriptor(Concrete, 'PUB'), desc(1));
+	assertEquals(Object.getOwnPropertyDescriptor(Concrete, 'PRO'), desc(2));
+	assertEquals(Object.getOwnPropertyDescriptor(Concrete, 'PRI'), desc(3));
 });
